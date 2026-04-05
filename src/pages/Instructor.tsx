bd3d24cbd680113e91cf7ddded233f20094426ba
kollobeit3alem <kollobeit3alem@outlook.com>
2026-04-05 16:48:20 +0200

    Update Instructor.tsx

--- src/pages/Instructor.tsx
+++ src/pages/Instructor.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth, apiCall } from '@/contexts/AuthContext';
 import { toast } from 'sonner';
-import type { Course, Lesson, QuizQuestion, User, StudentReport } from '@/types';
+import type { Course, Lesson, QuizQuestion, User } from '@/types';
 
 type TabType = 'courses' | 'lessons' | 'quizzes' | 'users';
 
