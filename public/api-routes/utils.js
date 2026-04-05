b0e8da5427ee2191bd4cef71646c9fcc68fb658e
kollobeit3alem <kollobeit3alem@outlook.com>
2026-04-05 04:27:50 +0200

    Create utils.js

---
+++ public/api-routes/utils.js
@@ -0,0 +1,6 @@
+export const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS,DELETE,PUT",
+  "Access-Control-Max-Age": "86400",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+};
