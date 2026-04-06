088087af2e081a6e04db70686a190a5b8df596bb
kollobeit3alem <kollobeit3alem@outlook.com>
2026-04-06 20:26:11 +0200

    Update utils.js

--- public/api-routes/utils.js
+++ public/api-routes/utils.js
@@ -1,5 +1,5 @@
 export const corsHeaders = {
-  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Origin": "https://kollobeit3alem.pages.dev",
   "Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS,DELETE,PUT",
   "Access-Control-Max-Age": "86400",
   "Access-Control-Allow-Headers": "Content-Type, Authorization",
